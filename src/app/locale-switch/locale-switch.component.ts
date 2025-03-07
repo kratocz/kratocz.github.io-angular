d33ea1fdf4ce39b0f12c5eede8c98f3ddbac0d5e
Petr Kratochvíl <krato@krato.cz>
2025-03-07 06:13:12 +0100

    <locale-switch>

--- src/app/locale-switch/locale-switch.component.ts
+++ src/app/locale-switch/locale-switch.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { LanguageService } from '../../i18n/language.service';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf, NgOptimizedImage, NgSwitch, NgSwitchCase} from '@angular/common';
 import {Router, RouterLink} from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'locale-switch',
@@ -9,14 +10,19 @@ import {Router, RouterLink} from '@angular/router';
   styleUrl: './locale-switch.component.scss',
   imports: [
     NgForOf,
-    RouterLink
+    RouterLink,
+    NgOptimizedImage,
+    NgIf,
+    NgSwitch,
+    NgSwitchCase
   ],
 })
 export class LocaleSwitchComponent {
   constructor(
     public languageService: LanguageService,
+    public translate: TranslateService,
     public router: Router,
-              ) {}
+  ) {}
 
   protected readonly LanguageService = LanguageService;
 }
